7ee2f3d576c1df5ad74aba90f35186fd3ff9aaee
Steven Bryant <stevelad_83@yahoo.com>
2023-01-12 12:28:12 -0500

    catchphrase input

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Select from './Select.js';
 
-export default function Form({ head, shirt, pants }) {
+export default function Form({ head, shirt, pants, input }) {
   return (
     <div className="form">
       <Select label="Head" options={['bird', 'duck', 'dog']} value={head} />
-      <Select label="Shirt" options={['white', 'black', 'rainbow']} value={head} />
-      <Select label="Pants" options={['trousers', 'gown', 'kilt']} value={head} />
+      <Select label="Shirt" options={['white', 'black', 'rainbow']} value={shirt} />
+      <Select label="Pants" options={['trousers', 'gown', 'kilt']} value={pants} />
+      <div className="form-control">
+        <input type="text" value={input} />
+        <label>Add a catchphrase</label>
+        <button>Add</button>
+      </div>
     </div>
   );
 }
