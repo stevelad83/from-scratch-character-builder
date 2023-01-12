ee60717a4dd9f0877e0639af175da571d2fac7b5
Steven Bryant <stevelad_83@yahoo.com>
2023-01-11 20:17:10 -0500

    built out App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React from 'react';
 import './App.css';
+import Form from './components/Form';
+import Stats from './components/Stats';
+import Avatar from './components/Avatar';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <main className="App">
+      <h1>Character Designer</h1>
+      <section className="container">
+        <div className="left">
+          <Form />
+          <Stats />
+        </div>
+        <div className="right">
+          <Avatar />
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default App;
